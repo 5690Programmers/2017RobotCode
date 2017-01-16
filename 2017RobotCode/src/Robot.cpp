4ccbc96c48d76be7b460ed4f81a461b510be3783
Ian R <Irossetto264@gmail.com>
2017-01-16 15:50:20 -0600

    Robot Code 2017
    Main Code

---
+++ 2017RobotCode/src/Robot.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <Joystick.h>
+#include <SampleRobot.h>
+#include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
+#include <RobotDrive.h>
+#include <Timer.h>
+
+
+class Robot: public frc::SampleRobot {
+	frc::RobotDrive myRobot { 0, 1 };
+	frc::Joystick stick { 0 };
+	frc::SendableChooser<std::string> chooser;
+	const std::string autoNameDefault = "Default";
+	const std::string autoNameCustom = "My Auto";
+
+public:
+	Robot() {
+
+		myRobot.SetExpiration(0.1);
+	}
+
+	void RobotInit() {
+		chooser.AddDefault(autoNameDefault, autoNameDefault);
+		chooser.AddObject(autoNameCustom, autoNameCustom);
+		frc::SmartDashboard::PutData("Auto Modes", &chooser);
+	}
+
+
+	void Autonomous() {
+		auto autoSelected = chooser.GetSelected();
+
+		std::cout << "Auto selected: " << autoSelected << std::endl;
+
+		if (autoSelected == autoNameCustom) {
+			// Custom Auto goes here
+			std::cout << "Running custom Autonomous" << std::endl;
+			myRobot.SetSafetyEnabled(false);
+			myRobot.Drive(-0.5, 1.0); // spin at half speed
+			frc::Wait(2.0);                // for 2 seconds
+			myRobot.Drive(0.0, 0.0);  // stop robot
+		} else {
+			// Default Auto goes here
+			std::cout << "Running default Autonomous" << std::endl;
+			myRobot.SetSafetyEnabled(false);
+			myRobot.Drive(-0.5, 0.0); // drive forwards half speed
+			frc::Wait(2.0);                // for 2 seconds
+			myRobot.Drive(0.0, 0.0);  // stop robot
+		}
+	}
+
+	/*
+	 * Runs the motors with arcade steering.
+	 */
+	void OperatorControl() override {
+		myRobot.SetSafetyEnabled(true);
+		while (IsOperatorControl() && IsEnabled()) {
+			// drive with arcade style (use right stick)
+			myRobot.ArcadeDrive(stick);
+
+			// wait for a motor update time
+			frc::Wait(0.005);
+		}
+	}
+
+	/*
+	 * Runs during test mode
+	 */
+	void Test() override {
+
+	}
+};
+
+START_ROBOT_CLASS(Robot)
