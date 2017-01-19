28fbd3f9c365294564b27a03c12f148ae9cd0f40
Ian R <Irossetto264@gmail.com>
2017-01-18 21:15:31 -0600

    Adjustments, Shifters, Kickers

--- 2017RobotCode/src/Robot.cpp
+++ 2017RobotCode/src/Robot.cpp
@@ -22,8 +22,8 @@
 class Robot: public frc::SampleRobot {
 	//Driving
 	frc::RobotDrive myRobot { 0, 1, 2, 3 };
-	frc::Joystick stick { 0 };
-	frc::XboxController Xbox { 1 };
+	frc::Joystick stick { 1 };
+	frc::XboxController Xbox { 0 };
 	//Motors and Stuff
 	frc::VictorSP Kicker { 0 };
 	frc::VictorSP Shooter { 1 };
@@ -80,21 +80,12 @@ public:
 	// FIXED: Vision Tracking on Bumpers and Shooting Mechanisms(Kicker and BallShooter) on respective Triggers
 
 	//**DRIVER PREF**
-	//Agitator on Y
-	//Climber on B
+	//Agitator on A
+	//Climber on Start/Back
 	//Shifting on X
-	//Intake on A
+	//Intake on Y
 	//Left or Right Joystick for driving
-	//Possible HomeButton to 0* robot...
 
-	// Correct Format Complete (JoystickHand hand)			            }
-	//if (Xbox.GetBumper(JoystickHand hand) const override
-	//			{
-	//				Shooter.Set(1);
-	//				Shooter.Set(-1);
-	//				Wait(0.1);
-	//			Shooter.Set(0);
-	//			}
 
 
 	void OperatorControl() override
@@ -103,75 +94,89 @@ public:
 		while (IsOperatorControl() && IsEnabled())
 		{
 		// drive with arcade style (use right stick)
-		myRobot.ArcadeDrive(stick);
+		myRobot.ArcadeDrive(Xbox);
 
 		// wait for a motor update time
 		frc::Wait(0.005);
 
-		//Fix so kicker kicks at X degrees and returns
-		if (Xbox.GetTriggerAxis(XboxController::JoystickHand(0))) //Left Trigger
-		{
-			Kicker.Set(-1);
-			Wait(0.1);
-			Kicker.Set(0);
-		}
+		//NOT TESTED
+		//Kicker Piston for Gear
+		if (Xbox.GetTriggerAxis(XboxController::JoystickHand(0)))
+			{
+				Kicker.Set(DoubleSolenoid::Value(1));
+				Wait(0.005);
+			}
+		else
+			{
+				Kicker.Set(DoubleSolenoid::Value(2));
+				Wait(0.005);
+			}
 		//Vision Tracking for Gear
 		if (Xbox.GetBumper(XboxController::JoystickHand(0)))
-		{
+			{
 
-		}
+			}
 
 		//Shooter Trigger
 		if (Xbox.GetTriggerAxis(XboxController::JoystickHand(1)))
-		{
-				Shooter.Set(1);
-				Wait(0.1);
-				Shooter.Set(0);
+			{
+					Shooter.Set(1);
+					Wait(0.1);
+					Shooter.Set(0);
 
-		}
+			}
 		//Vision Tracking for Shooter
 		if (Xbox.GetBumper(XboxController::JoystickHand(1)))
-		{
+			{
 
-		}
+			}
 
 
+		//Hooper Alligator ** Firmly cup the ball**
+		if (Xbox.GetAButton())
+			{
+				Agitator.Set(0.2);
+				Wait(0.1);
+				Agitator.Set(0);
+			}
 
-		if (Xbox.GetAButton()) //Hooper Alligator
-		{
-			Agitator.Set(-1);
-			Agitator.Set(1);
-			Wait(0.1);
-			Agitator.Set(0);
-		}
+		//Climber Down
+		if (Xbox.GetBackButton())
+			{
+				Climber.Set(-1);
+				Wait(0.1);
+				Climber.Set(0);
+			}
 
-		if (Xbox.GetBButton()) //Climber
-		{
-			Climber.Set(-1);
-			Climber.Set(1);
-			Wait(0.1);
-			Climber.Set(0);
-		}
+		//Climber Up
+		if (Xbox.GetStartButton())
+			{
+				Climber.Set(1);
+				Wait(0.1);
+				Climber.Set(0);
+			}
 
-		if (Xbox.GetXButton()&& Shifter1.Get()!=0&& Shifter2.Get()!=0) //Shifter
-		{
-			Shifter1.Set(DoubleSolenoid::Value(1));
-			Shifter2.Set(DoubleSolenoid::Value(1));
-			Wait(0.005);
-		}else
-		{
-			Shifter1.Set(DoubleSolenoid::Value(2));
-			Shifter2.Set(DoubleSolenoid::Value(2));
-			Wait(0.005);
-		}
-		if (Xbox.GetYButton()) //Intake
-		{
-			Intake.Set(-1);
-			Intake.Set(1);
-			Wait(0.005);
-			Intake.Set(0);
+		//Shifter
+		if (Xbox.GetXButton()&& Shifter1.Get()!=0&& Shifter2.Get()!=0)
+			{
+				Shifter1.Set(DoubleSolenoid::Value(1));
+				Shifter2.Set(DoubleSolenoid::Value(1));
+				Wait(0.005);
+			}
+		else
+			{
+				Shifter1.Set(DoubleSolenoid::Value(2));
+				Shifter2.Set(DoubleSolenoid::Value(2));
+				Wait(0.005);
+			}
 
-		}
+		 //Intake
+		if (Xbox.GetYButton())
+			{
+				Intake.Set(-1);
+				Wait(0.005);
+				Intake.Set(0);
+			}
 
 		Wait(0.005);
 	}
